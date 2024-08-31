e0eef5f897a77a4bdbd517f65ddfe412246de6ab
toriza <zgtori@gmail.com>
2024-08-31 09:32:25 +0200

    new file upload

---
+++ PracticeExerciseUserProfile.java
@@ -0,0 +1,8 @@
+//Prompt the user to enter their name and store it as a string.
+//Ask the user to input their age and store it as an integer.
+//Request the user's favorite hobby and store it as a string.
+//Print out a summary that includes the user's name, age, and hobby.
+
+public class PracticeExerciseUserProfile {
+
+}
